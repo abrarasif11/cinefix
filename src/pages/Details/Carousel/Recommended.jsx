f6f4a470dfe0334bde699982d71e5387251691b5
abrarasif11 <faasif929@gmail.com>
2023-09-14 00:31:58 +0600

    complete-details-section

---
+++ src/pages/Details/Carousel/Recommended.jsx
@@ -0,0 +1,21 @@
+import Carousel from "../../../components/Carousel/Carousel";
+import useFetch from "../../../hooks/useFetch";
+
+
+
+const Recommended = ({ mediaType, id }) => {
+    const { data, loading, error } = useFetch(
+        `/${mediaType}/${id}/recommendations`
+    );
+
+    return (
+        <Carousel
+            title="Recommendations"
+            data={data?.results}
+            loading={loading}
+            endpoint={mediaType}
+        />
+    );
+};
+
+export default Recommended;
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,21 +1,25 @@
-import {  useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from './DetailBanner/DetailsBanner';
 import './Details.scss'
 import Cast from './Cast/Cast';
 import VideoSection from './VideoSections/VideoSection';
+import Similar from './Carousel/Similar';
+import Recommended from './Carousel/Recommended';
 
 const Details = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
   const { data: credits, loading: creditsLoading } = useFetch(
-      `/${mediaType}/${id}/credits`
+    `/${mediaType}/${id}/credits`
   );
   return (
     <div>
-      <DetailsBanner  video={data?.results?.[0]} crew={credits?.crew}/>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
       <VideoSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommended mediaType={mediaType} id={id} />
     </div>
   )
 }
